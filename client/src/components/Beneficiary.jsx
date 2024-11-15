fb969a91c93b896142f8d2a42757efb61fa23783
Naz <mercy.kamau@student.moringaschool.com>
2024-11-15 18:15:11 +0300

    Beneficiary,Charity and Stories

---
+++ client/src/components/Beneficiary.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addBeneficiary } from './beneficiariesSlice';
+
+const Beneficiaries = () => {
+  const beneficiaries = useSelector((state) => state.beneficiaries);
+  const dispatch = useDispatch();
+
+  const [newBeneficiary, setNewBeneficiary] = useState({
+    id: Date.now(),
+    name: '',
+    school: '',
+    age: '',
+    dateJoined: '',
+  });
+
+  const handleChange = (e) => {
+    setNewBeneficiary({ ...newBeneficiary, [e.target.name]: e.target.value });
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    dispatch(addBeneficiary(newBeneficiary));
+    setNewBeneficiary({ id: Date.now(), name: '', school: '', age: '', dateJoined: '' });
+  };
+
+  return (
+    <div>
+      <h2>Beneficiaries</h2>
+      <ul>
+        {beneficiaries.map((beneficiary) => (
+          <li key={beneficiary.id}>
+            {beneficiary.name} - {beneficiary.school}
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleAdd}>
+        <input name="name" placeholder="Name" onChange={handleChange} required />
+        <input name="school" placeholder="School" onChange={handleChange} required />
+        <input name="age" type="number" placeholder="Age" onChange={handleChange} required />
+        <input name="dateJoined" type="date" onChange={handleChange} required />
+        <button type="submit">Add Beneficiary</button>
+      </form>
+    </div>
+  );
+};
+
+export default Beneficiaries;
