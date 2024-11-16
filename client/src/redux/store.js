4fd25112fce8da5fec98b85b5d89fdfdadf112b9
avant muchiri <avant.muchiri@student.moringaschool>
2024-11-16 03:27:00 +0300

    redux files

---
+++ client/src/redux/store.js
@@ -0,0 +1,13 @@
+// src/redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import donorReducer from './slices/donorSlice';
+import adminReducer from './slices/adminSlice';
+import charityReducer from './slices/charitySlice';
+
+export const store = configureStore({
+  reducer: {
+    donor: donorReducer,
+    admin: adminReducer,
+    charity: charityReducer,
+  },
+});
